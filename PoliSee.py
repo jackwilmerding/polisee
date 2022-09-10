75ae957c77472a4d99121377c741ebfed745b178
Jack Wilmerding <jwilmerding23@gwu.edu>
2022-09-10 17:46:43 -0400

    Databasing
    Working on databasing (WIP)

--- PoliSee.py
+++ PoliSee.py
@@ -1,13 +1,19 @@
 from requests import *
+import pymongo
 
 key = ""
-congress = 117
+chamber = "s"
+
+class Congress:
+    def __init__(self, congressNumber):
+        self.congressNumber = congressNumber
+        self.members = get_members()
 
 def get_secrets(filename: str):
     with open(filename) as file:
         return file.readline()
 
-def get_bills():
+def get_bills(congressNumber):
     params = {
         "api_key": key,
         "format": "json",
@@ -15,7 +21,7 @@ def get_bills():
         "limit": 250
     }
     bills = []
-    url = f"https://api.congress.gov/v3/bill/{congress}/s"
+    url = f"https://api.congress.gov/v3/bill/{congressNumber}/{chamber}"
     response = get(url, params).json()
     while len(response["bills"]) != 0:
         response = get(url, params).json()
@@ -24,6 +30,14 @@ def get_bills():
         params["offset"] += 250
     return bills
 
+def get_members(congressNumber):
+    return {}
+
+def update():
+    for congressNumber in [117, 116, 115, 114, 113]:
+        return
+
+
+
 if __name__ == "__main__":
     key = get_secrets("./secrets.txt")
-    bills = get_bills()

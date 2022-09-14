25b7c5f7f8bf5f59e1ddc5b8ab79a7a0ba1331f8
Jack Wilmerding <61218012+jackwilmerding@users.noreply.github.com>
2022-09-13 21:45:28 -0400

    Getting there

--- PoliSee.py
+++ PoliSee.py
@@ -4,7 +4,7 @@ import time
 
 key = ""
 #TODO Expand for production
-congresses = [117]
+congresses = [116]
 mongo = None
 db = None
 collection = None
@@ -33,7 +33,7 @@ def get_until_success(endpoint, params):
         time.sleep(60)
         req = get(endpoint, params).json()
     time.sleep(1.2)
-    return(req.json())
+    return req.json()
 
 
 #DONE
@@ -48,7 +48,6 @@ def get_bills(congress_number):
     bills = []
     url = f"https://api.congress.gov/v3/bill/{congress_number}/hr"
     response = get_until_success(url, params)
-
     request_counter += 1
     while len(response["bills"]) != 0:
         bills.extend(response["bills"])
@@ -70,23 +69,24 @@ def get_bills(congress_number):
 
 
 #DONE
-def update_edge(congress_number: int, from_node: str, to_node: str):
+def update_edge(congress_number: int, from_node: str, to_node: str, chamber: str):
     collection = db[str(congress_number) + "_edges"]
     edge_document = collection.find_one({"$and": [{"from_node": from_node}, {"to_node": to_node}]})
     if edge_document != None:
         current_count = edge_document["count"]
-        collection.update_one({"$and": [{"from_node": from_node}, {"to_node": to_node}]}, {"$set": {"count" : current_count + 1}})
+        collection.update_one({"$and": [{"from_node": from_node}, {"to_node": to_node}, {"chamber" : chamber}]}, {"$set": {"count" : current_count + 1}})
     else:
         doc = {}
         doc["_id"] = from_node + "," + to_node
         doc["from_node"] = from_node
         doc["to_node"] = to_node
+        doc["chamber"] = chamber
         doc["count"] = 1
         collection.insert_one(doc)
 
 
 #DONE
-def update_node(congress_number: int, bioguide_id: str, first_name: str, last_name: str, state: str, party: str, type: str):
+def update_node(congress_number: int, bioguide_id: str, first_name: str, last_name: str, state: str, party: str, chamber: str):
     collection = db[str(congress_number) + "_nodes"]
     node_document = collection.find_one({"_id": bioguide_id})
     doc = {}
@@ -95,46 +95,46 @@ def update_node(congress_number: int, bioguide_id: str, first_name: str, last_na
     doc["last_name"] = last_name.capitalize()
     doc["state"] = state.upper()
     doc["party"] = party[:1]
-    doc["type"] = type
+    doc["chamber"] = chamber
     if node_document != None:
         collection.replace_one({"_id": bioguide_id}, doc)
     else:
         collection.insert_one(doc)
 
 
-def init():
+#DONE
+def get_bill_info(bill, congress_number):
     global request_counter
     params = {
         "api_key": key,
         "format": "json"
     }
-    #TODO CLEAN UP
-    for congress_number in congresses:
-        bills = get_bills(congress_number)
-        ctr = 0
-        for bill in bills[0:5]:
-            ctr += 1
-            print(f"\rProcessing bills: {ctr}/{len(bills)}; {request_counter} requests", end = "")
-            bill_type = bill["type"]
-            bill_number = bill["number"]
-            current_sponsor = get(f"https://api.congress.gov/v3/bill/{congress_number}/{bill_type}/{bill_number}", params).json()["bill"]["sponsors"][0]
-            current_cosponsors = get(f"https://api.congress.gov/v3/bill/{congress_number}/{bill_type}/{bill_number}/cosponsors", params).json()["cosponsors"]
-            request_counter += 2
-            if bill_type == "S":
-                member_type = "Senator"
-            elif bill_type == "HR":
-                member_type = "Representative"
-            if current_sponsor["bioguideId"] not in current_congress.members:
-                current_congress.members[current_sponsor["bioguideId"]] = Member(current_sponsor["bioguideId"], "Hon. " + current_sponsor["firstName"].capitalize() + " " + current_sponsor["lastName"].capitalize(), current_sponsor["state"], current_sponsor["party"], member_type)
-            for cosponsor in current_cosponsors:
-                if cosponsor["bioguideId"] not in current_congress.members[current_sponsor["bioguideId"]].colleagues:
-                    current_congress.members[current_sponsor["bioguideId"]].colleagues[cosponsor["bioguideId"]] = 1
-                else:
-                    current_congress.members[current_sponsor["bioguideId"]].colleagues[cosponsor["bioguideId"]] += 1
-        collection.insert_many(current_congress.pack())
+    bill_type = bill["type"]
+    bill_number = bill["number"]
+    current_sponsor = get_until_success(f"https://api.congress.gov/v3/bill/{congress_number}/{bill_type}/{bill_number}", params)["bill"]["sponsors"][0]
+    current_cosponsors = get_until_success(f"https://api.congress.gov/v3/bill/{congress_number}/{bill_type}/{bill_number}/cosponsors", params)["cosponsors"]
+    if bill_type == "S":
+        current_sponsor["chamber"] = "Senate"
+    elif bill_type == "HR":
+        current_sponsor["chamber"] = "House of Representatives"
+    request_counter += 2
+    return current_sponsor, current_cosponsors
+
+#DONE
+def get_congress_data(congress_number):
+    global request_counter
+    bills = get_bills(congress_number)
+    ctr = 0
+    for bill in bills:
+        ctr += 1
+        print(f"\rProcessing bills: {ctr}/{len(bills)}; {request_counter} requests", end="")
+        current_sponsor, current_cosponsors = get_bill_info(bill, congress_number)
+        for cosponsor in current_cosponsors:
+            update_node(congress_number, current_sponsor["bioguideId"], current_sponsor["firstName"], current_sponsor["lastName"], current_sponsor["state"], current_sponsor["party"], current_sponsor["chamber"])
+            update_edge(congress_number, current_sponsor["bioguideId"], cosponsor["bioguideId"], current_sponsor["chamber"])
 
 
 if __name__ == "__main__":
     start = time.time()
     get_secrets("./secrets.txt")
-    update_edge(117, "12244l", "sdafdas")
+    get_congress_data(116)
